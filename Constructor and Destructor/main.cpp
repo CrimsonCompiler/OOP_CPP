259990ea53d3c891313f4e56949a8ea572891b51
CrimsonCompiler <toisiftasrik@gmail.com>
2026-03-08 05:23:41 +0600

    feat: add Customer class demonstrating default and parameterized constructors.

--- Constructor and Destructor/main.cpp
+++ Constructor and Destructor/main.cpp
@@ -1,7 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    cout << "I will be learning constructor and destructor" << endl;
+class Customer {
+  string name;
+  int accountNumber;
+  int balance;
+
+public:
+  Customer() {
+    // Can't be seen but automatically written by default constructor
+    cout << "Default constructor called" << endl;
+  }
+
+  // Parameterized constructor
+  Customer(string name, int accountNumber, int balance) {
+    this->name = name;
+    this->accountNumber = accountNumber;
+    this->balance = balance;
+  }
+};
+
+int main() {
+  Customer A1;
+  Customer B1("Tousif", 45679, 10000);
 }
+
+// Constructor dosen't have any return type
+// By default constructor is private
+// This is a pointer to the current object
+// this->name = name;
